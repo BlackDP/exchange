8485073b95341c9a79c0732539a060f6a9fe9985
Vlad <blackdp@ya.ru>
2021-12-14 03:44:08 +0300

    Добавлена функция загрузки файла

--- exch/views.py
+++ exch/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.core.files.storage import default_storage
 
 
 
 
+from .parser import proccesing_uploaded_file
 from .forms import UploadFileForm
 # Create your views here.
 
@@ -15,19 +16,26 @@ def index(request):
 
 
 def upload_file(request):
-    form = UploadFileForm()
-    return render(request, 'exch/upload.html', {'form': form})
-
-""" def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
-            proccesing_uploaded_file(request.FILES['file'])
-            return HttpResponseRedirect('/success/url/')
+            file = request.FILES['file']
+            file_name = default_storage.save(file.name, file)
+            proccesing_uploaded_file(file_name)
+            return HttpResponse(f'Загружен {file_name}')
+        
     else:
         form = UploadFileForm()
-    return render(request, 'exch/upload.html', {'form': form}) """
+    return render(request, 'exch/upload.html', {'form': form})
+
+import xml.etree.ElementTree as ET
 
 
-def proccesing_uploaded_file():
-    pass
+
+
+
+def proccesing_uploaded_file(xmlfile):
+    tree = ET.parse(xmlfile)
+    root_xml = tree.getroot()
+    for child in root_xml.iter('name'):
+        print(child.text)
